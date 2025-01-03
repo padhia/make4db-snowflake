f25b71f1ee848265a22532979cffe23e5d24ae71
Paresh Adhia <padhia@gmail.com>
2025-01-03 17:29:20 -0500

    added auto dependencies for stream objects

--- make4db-snowflake.nix
+++ make4db-snowflake.nix
@@ -10,7 +10,7 @@
 }:
 buildPythonPackage {
   pname = "make4db-snowflake";
-  version = "0.1.0";
+  version = "0.1.1";
   pyproject = true;
 
   src = ./.;
--- make4db_snowflake.py
+++ make4db_snowflake.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from functools import cache
 from itertools import islice
 from textwrap import dedent
-from typing import Any, Iterable, Self, TextIO, cast
+from typing import Any, Iterable, NamedTuple, Self, TextIO, cast
 
 from make4db.provider import DbAccess, DbProvider, Feature, PySqlFn, SchObj
 from sfconn import getsess, pytype
@@ -17,7 +17,20 @@ from yappt.grid import AsciiBoxStyle
 
 logger = logging.getLogger(__name__)
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+
+
+class ObjInfo(NamedTuple):
+    obj: SchObj
+    kind: str
+    args: str | None
+
+    def __str__(self) -> str:
+        if self.args is not None:
+            args = "(" + self.args.split("(", 1)[1].rsplit(")", 1)[0] + ")" if "(" in self.args else ""
+            return f"{self.obj}{args}"
+
+        return str(self.obj)
 
 
 @dataclass
@@ -53,25 +66,67 @@ class SfAcc(DbAccess):
             )
 
     def iterdep(self, objs: Iterable[SchObj]) -> Iterable[tuple[SchObj, SchObj]]:
-        obj_lit = ",\n".join((f"('{o.sch.upper()}', '{o.obj.upper()}')" for o in objs))
+        all_objs = set(objs)
 
-        sql = dedent(
-            f"""\
-            with objs(sch_name, obj_name) as (
-                select *
-                from values {indent(obj_lit, 4)}
+        def obj_deps() -> Iterable[tuple[SchObj, SchObj]]:
+            obj_lit = ",\n".join((f"('{o.sch.upper()}', '{o.obj.upper()}')" for o in all_objs))
+
+            sql = dedent(
+                f"""\
+                with objs(sch_name, obj_name) as (
+                    select *
+                    from values {indent(obj_lit, 4)}
+                )
+                select distinct referencing_schema, referencing_object_name, referenced_schema, referenced_object_name
+                from snowflake.account_usage.object_dependencies d
+                join objs o on referencing_database = current_database()
+                    and referenced_database = current_database()
+                    and referencing_schema = o.sch_name
+                    and referencing_object_name = o.obj_name"""
             )
-            select distinct referencing_schema, referencing_object_name, referenced_schema, referenced_object_name
-            from snowflake.account_usage.object_dependencies d
-            join objs o on referencing_database = current_database()
-                and referenced_database = current_database()
-                and referencing_schema = o.sch_name
-                and referencing_object_name = o.obj_name"""
-        )
 
-        with self.sess.connection.cursor() as csr:
-            csr.execute(sql)
-            yield from ((SchObj(r[0], r[1]), SchObj(r[2], r[3])) for r in cast(Iterable[tuple[str, str, str, str]], csr))
+            with self.sess.connection.cursor() as csr:
+                csr.execute(sql)
+                yield from ((SchObj(r[0], r[1]), SchObj(r[2], r[3])) for r in cast(Iterable[tuple[str, str, str, str]], csr))
+
+        def stream_deps() -> Iterable[tuple[SchObj, SchObj]]:
+            def parse_objname(t: str) -> tuple[str, str, str] | None:
+                try:
+                    db, sch, tb = t.split(".")
+                    return (db, sch, tb)
+                except ValueError:
+                    return None
+
+            with self.sess.connection.cursor() as csr:
+                csr.execute("SHOW STREAMS IN DATABASE")
+                name_xref = {d.name: e for e, d in enumerate(csr.description)}
+                cols = [name_xref["database_name"], name_xref["schema_name"], name_xref["name"], name_xref["table_name"]]
+                deps = ((r[cols[0]], SchObj(r[cols[1]], r[cols[2]]), r[cols[3]]) for r in cast(Iterable[list[str]], csr))
+                for g_db, g_obj, d_name in deps:
+                    if g_obj in all_objs and (d := parse_objname(d_name)) is not None and d[0] == g_db:
+                        yield (g_obj, SchObj(d[1], d[2]))
+
+        yield from obj_deps()
+        yield from stream_deps()
+
+    def drop_except(self, objs: set[SchObj]) -> Iterable[str]:
+        def sch_objs(sch: str) -> list[ObjInfo]:
+            def get_objs(kind: str):
+                with self.sess.connection.cursor() as csr:
+                    csr.execute(f"show terse {kind}s in schema {sch}")
+                    return [ObjInfo(SchObj.make(sch, r[1]), kind, None) for r in csr]  # type: ignore
+
+            def get_fn(kind: str):
+                with self.sess.connection.cursor() as csr:
+                    csr.execute(f"show user {kind}s in schema {sch}")
+                    return [ObjInfo(SchObj.make(sch, r[1]), kind, r[8]) for r in csr]  # type: ignore
+
+            return get_objs("table") + get_objs("view") + get_objs("stream") + get_fn("function") + get_fn("procedure")
+
+        for sch in set(o.sch for o in objs):
+            for oi in sch_objs(sch):
+                if oi.obj not in objs:
+                    yield f"drop {oi.kind} {oi}"
 
 
 @dataclass
